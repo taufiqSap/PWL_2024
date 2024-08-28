2f66cab3756a206563eceba23f495d947afbfb42
taufiqSap <taufiqsatriaji@gmail.com>
2024-08-28 22:19:36 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS1</title>
+    <h1>SELAMAT DATANG</h1>
+<h2>BIODATA </h2>
+</head>
+<body>
+        <h3>PROVINSI JAWA TIMUR</h3>
+        <h3>KOTA BLITAR</h3>
+        
+        <table>
+            <tr>
+                <td>NIK</td>
+                <td>: 022038473*****</td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>: TAUFIQ SATRIAJI ARIYA PUTRA RIYADI</td>
+            </tr>
+            <tr>
+                <td>Tempat/Tgl Lahir</td>
+                <td>: BLITAR, 28-06-2003</td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>: LAKI-LAKI</td>
+            </tr>
+            <tr>
+                <td>Gol. Darah</td>
+                <td>: -</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>: JL. CAKRANINGRAT NO 17</td>
+            </tr>
+            <tr>
+                <td>RT/RW</td>
+                <td>: 003/004</td>
+            </tr>
+            <tr>
+                <td>Kel/Desa</td>
+                <td>: SENTUL;</td>
+            </tr>
+            <tr>
+                <td>Kecamatan</td>
+                <td>: KEPANJEN KIDUL</td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>: ISLAM</td>
+            </tr>
+            <tr>
+                <td>Status Perkawinan</td>
+                <td>: BELUM KAWIN</td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>: PELAJAR/MAHASISWA</td>
+            </tr>
+            <tr>
+                <td>Kewarganegaraan</td>
+                <td>: WNI</td>
+            </tr>
+            <tr>
+                <td>Berlaku Hingga</td>
+                <td>: SEUMUR HIDUP</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
